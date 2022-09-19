e1962bbc4fe5045c856acab05a5806b1eec0e7fd
aks3632 <aks3632@mavs.uta.edu>
2022-09-19 15:13:24 -0500

    P04 1st Commit Progress GasVehicle OK

---
+++ P04/full_credit/GasVehicle.java
@@ -0,0 +1,28 @@
+public class GasVehicle extends Vehicle {
+
+  // Constructor
+  public GasVehicle(int year, String make, String model, BodyStyle bodystyle, double milesPerGallon, double gallonsInTank) {
+    super(year, make, model, bodystyle); // call to super must be first statement in constructor
+    this.milesPerGallon = milesPerGallon;
+    this.gallonsInTank = gallonsInTank;
+  }
+
+  // Methods
+  public double range() {
+    return 1.0;
+
+  };
+  public double fuelConsumed(double miles) {
+    return 2.0;
+
+  };
+  public double dollarsToTravel(double miles) {
+    return 3.0;
+  };
+
+  // Fields
+  public static double dollarsPerGallonOfGas;
+  private double milesPerGallon;
+  private double gallonsInTank;
+
+}

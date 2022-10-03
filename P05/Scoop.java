129d72da03d43c5e0c531d15d67096128a149e05
aks3632 <aks3632@mavs.uta.edu>
2022-10-03 16:37:21 -0500

    P05 Task1 MXF mv Mice.java Scoop

---
+++ P05/Scoop.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.*;
+
+public class Scoop{
+  public static void main(String[] args) { // function main begins program execution
+
+  } // end main()
+}

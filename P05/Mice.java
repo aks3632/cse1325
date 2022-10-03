c05e6680fa1bfb5d3b746a3c1aed76a80b29a54b
aks3632 <aks3632@mavs.uta.edu>
2022-10-03 06:31:13 -0500

    Task1 MX InWork

---
+++ P05/Mice.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.*;
+
+public class Mice{
+  public static void main(String[] args) { // function main begins program execution
+
+  } // end main()
+}

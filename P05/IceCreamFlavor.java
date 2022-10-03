706f2e03176754b47ab4f23d074937df51733156
aks3632 <aks3632@mavs.uta.edu>
2022-10-03 13:42:23 -0500

    P05 Task1 MXF MixInFlavor & IceCreamFlavor

---
+++ P05/IceCreamFlavor.java
@@ -0,0 +1,9 @@
+public class IceCreamFlavor extends Item {
+
+  // Constructor
+  IceCreamFlavor (String name, String description, int cost, int price) {
+    super(name, description, cost, price);
+    /* Item = Superclass; therefore, see above */
+  } // end IceCreamFlavor
+
+}

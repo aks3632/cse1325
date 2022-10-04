18e4fff44a1bc64f739d45a76a122c6cb099e0e3
aks3632 <aks3632@mavs.uta.edu>
2022-10-03 20:31:55 -0500

    P05 Task4 MixIn resolved

--- P05/MixIn.java
+++ P05/MixIn.java
@@ -1,3 +1,24 @@
 public class MixIn {
-  
+
+  // Constructor
+  MixIn(MixInFlavor flavor, MixInAmount amount) {
+    this.flavor = flavor;
+    this.amount = amount;
+  } // end Constructor
+
+  @Override
+  public String toString() {
+
+    if (amount!=(MixInAmount.Normal)) {
+      return " " + flavor;
+    }
+    else {
+      return flavor + "(" + amount + ")";
+    } // end if else
+  } // end toString
+
+  // Two private Fields
+  private MixInFlavor flavor;
+  private MixInAmount amount;
+
 }
--- P05/Scoop.java
+++ P05/Scoop.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.*;
 
-public class Scoop{
+public class Scoop {
   public static void main(String[] args) { // function main begins program execution
 
   } // end main()

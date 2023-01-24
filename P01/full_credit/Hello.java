1af5a69aac5dfe882b423859f3dfa3d24900239d
aks3632 <aks3632@mavs.uta.edu>
2023-01-24 03:50:47 -0600

    P01 First Commit 1-24-2023

--- P01/full_credit/Hello.java
+++ P01/full_credit/Hello.java
@@ -1,5 +1,11 @@
- public class Hello {
-   public static void main(String[] args) { // function main begins program execution
-     System.out.println("Hello, Ali Senkayi!");
-   }
- } // end main()
+import java.util.Scanner;
+import java.io.*;
+
+
+public class Hello {
+  public static void main (String[] args) { // function main() begins program execution
+    // Type indentifier = new Type()
+
+    System.out.println("Hello, Ali Senkayi!");
+  } // end main()
+} // end class

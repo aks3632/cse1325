f446d2dc4a188b3504e1165196d535e2afe57573
aks3632 <aks3632@mavs.uta.edu>
2023-02-01 18:23:47 -0600

    P02 Solution 1/31/2023

--- P02/full_credit/MyPets.java
+++ P02/full_credit/MyPets.java
@@ -1,10 +1,52 @@
-import java.lang.Enum;
 import java.util.Scanner;
+import java.io.*;
+import java.lang.Enum;
 
  public class MyPets {
    public static void main(String[] args) {
 
-     System.out.println("Hi");
+     // Object
+     // enum Type can be auto instnace EX) Type.DOG
+     Pet ani1 = new Pet("Tommy", 14, Type.RABBIT);
+     Pet ani2 = new Pet("Jim", 30, Type.PONY);
+     Pet ani3 = new Pet("Bob", 21, Type.SNAKE);
+     Pet ani4 = new Pet("Howard", 50, Type.DOG);
 
+     // System.out.println(ani1);
+     // System.out.println(ani2);
+     // System.out.println(ani3);
+     // System.out.println(ani4);
+
+     Pet[] kingdom = {ani1, ani2, ani3, ani4};
+
+     for( Pet i: kingdom) {
+       System.out.println(i);
+     }
    }
  }
+
+
+ /******************************************************************************
+
+ * CSE 1325-004 Spring 2023
+ * File: MyPets.java, Type.java, Pet.java, build.xml
+ * Author:
+ * Created on: Jan 1, 2023 5:00 AM
+ .*
+ * UTA Student Name:
+ * UTA ID:
+
+ *******************************************************************************/
+
+ /*
+
+Hints: ~ cse1325-prof/03/code_from_slides/date3
+Date.java - Bulk of program
+Month.java - enum
+Main.java - Interactive | Month acquired uniquely
+
+ */
+
+ /******************************************************************************
+ Java Programming HW P02
+ *******************************************************************************/
--- P02/full_credit/Pet.java
+++ P02/full_credit/Pet.java
@@ -7,16 +7,16 @@ public class Pet {
     this.type = type;
   } // end Pet constructor
 
-  // Tell Java how to convert an object of this type into a String
-  // The @Override helps us catch typographical errors - more later
-  @Override
-  public String toString() {   // Special Method
-    return name + age + type;
-  }
-
   private String name;
   private double age;
   private Type type;
+
+  // Tell Java how to convert an object of this type into a String
+  // The @Override helps us catch typographical errors - more later
+  @Override
+  public String toString() {
+    return " " + name + " " + "is a" + " " + type + " " + "age" + " " + age + ".";
+  }
   // private final double humanLifespan = 80;
 
 }

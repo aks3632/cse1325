c8f3cd5fafdbf5b3ed5c5623e7ce17d66071c1e3
aks3632 <aks3632@mavs.uta.edu>
2023-02-14 08:57:08 -0600

    PO4 TrialRun11 2/14/23 8:56AM

--- P04/full_credit/Store.java
+++ P04/full_credit/Store.java
@@ -28,9 +28,13 @@ public class Store {
     // System.out.println(products.get(0)); OR System.out.println(products.get(0).toString());
 
     Taxed.setTaxRate(0.0825);
+    Taxfree tfree = new Taxfree("Milk", 2.85);
+    tfree.price();
+
+    for(Product txCode : products)
+    System.out.println(txCode.price());
 
     // Stystem.out.println(products.price());
-
 
     int input = 0;
 
@@ -38,7 +42,7 @@ public class Store {
     System.out.println("Welcome to the Store");
     System.out.println("=====================\n");
     System.out.printf("Enter a choice: \n"); // Promt user
-    System.out.println("1. "  + products.get(0).toString());  // (M)ilk
+    System.out.println("1. "  + products.get(0).toString() + "\t" + tfree.price());  // (M)ilk
     System.out.println("2. "  + products.get(1).toString());  // (B)read
     System.out.println("3. "  + products.get(2).toString());  // (C)heese
     System.out.println("4. "  + products.get(3).toString());  // (E)ggs

57e4564af9c9bca90528499e3f550a5521b4be5a
aks3632 <aks3632@mavs.uta.edu>
2023-02-14 20:16:10 -0600

    PO4 TrialRun12 2/14/23 8:15PM

--- P04/full_credit/Store.java
+++ P04/full_credit/Store.java
@@ -8,11 +8,6 @@ public class Store {
   private ArrayList<Product> products = new ArrayList<>();
   private ArrayList<Product> shoppingCart = new ArrayList<>();
 
-
-  public void DisplayItems() {
-  }
-
-
   public int Menu() { // Function Method | Removed static from header
 
     products.add(new Taxfree("Milk", 2.85));        products.add(new Taxfree("Bread", 1.99));
@@ -24,24 +19,12 @@ public class Store {
         Arrays.asList(new Product(Milk, 2.85), new Product(Bread, 1.99),   new Product(cheese, .85),
                       new Product(Eggs, 6.95), new Product(Ice Cream, 4.95),  Product(Poptarts, 3.49)));
     */
-
     // System.out.println(products.get(0)); OR System.out.println(products.get(0).toString());
 
     Taxed.setTaxRate(0.0825);
     // Option 1
-    Taxfree tfree = new Taxfree("Milk", 2.85);
-    tfree.price();
-
-    int i = 0;
-    // Option 2 | For each loop
-    for(Product txCode : products) { // HINT via ArrayList
-      System.out.print(i + ". "  + products.get(i).toString());
-      System.out.println("" + "\t" + txCode.price());
-      i++;
-      if(i > 6) {
-        break;
-      } // end if
-    } // end for each loop
+    // Taxfree tfree = new Taxfree("Milk", 2.85);
+    // tfree.price();
 
     int input = 0;
 
@@ -49,14 +32,27 @@ public class Store {
     System.out.println("Welcome to the Store");
     System.out.println("=====================\n");
     System.out.printf("Enter a choice: \n"); // Promt user
-    System.out.println("0. "  + products.get(0).toString());  // (M)ilk || tfree.price()
-    System.out.println("1. "  + products.get(1).toString());  // (B)read
-    System.out.println("2. "  + products.get(2).toString());  // (C)heese
-    System.out.println("3. "  + products.get(3).toString());  // (E)ggs
-    System.out.println("4. "  + products.get(4).toString());  // (I)ce Cream
-    System.out.println("5. "  + products.get(5).toString());  // (P)optarts
-    System.out.println("6. "  + products.get(6).toString());  // (O)reos
-    System.out.println("7. (q)uit?\n");
+    // Option 2 | For each loop
+    int i = 0; // index count for items
+    for(Product txCode : products) { // HINT: derived via ArrayList
+      System.out.print(i + ". " + products.get(i).toString());
+      System.out.println("" + "\t\t\t" + String.format("$ %.2f", txCode.price()));
+      // System.out.println("" + "\t" + txCode.price());
+      i++;
+      if(i > 6) {
+        break;
+      } // end if
+    } // end for each loop
+    /* Substituted above
+    System.out.println("0. " + products.get(0).toString());  // (M)ilk || tfree.price()
+    System.out.println("1. " + products.get(1).toString());  // (B)read
+    System.out.println("2. " + products.get(2).toString());  // (C)heese
+    System.out.println("3. " + products.get(3).toString());  // (E)ggs
+    System.out.println("4. " + products.get(4).toString());  // (I)ce Cream
+    System.out.println("5. " + products.get(5).toString());  // (P)optarts
+    System.out.println("6. " + products.get(6).toString());  // (O)reos
+    */
+    System.out.println("7.  (q)uit?\n");
     System.out.printf("> Chose a number: ");
     Scanner number = new Scanner(System.in);
 
@@ -84,7 +80,6 @@ public class Store {
       // https://stackoverflow.com/questions/2559527/non-static-variable-cannot-be-referenced-from-a-static-context
       // can now access non-static fields
 
-
       char choice = (char)Menu(); // Obtains function Menu
 
       do {
@@ -95,43 +90,43 @@ public class Store {
         System.out.printf("\n");
         switch (choice) {
           case 0: // Dispaly: Dataset list
-              System.out.println("Option 1: Milk ($2.85)" + products.get(0).toString());
+              System.out.println("Option 0: Milk ($2.85)" + products.get(0).toString());
               System.out.printf("\n");
               /* Input | Add to shoppingCart if selected */
               break;
 
           case 1: // Display: 2nd choice
-              System.out.printf("Option 2: Bread ($1.99)\n" + products.get(1).toString());
+              System.out.println("Option 1: Bread ($1.99)" + products.get(1).toString());
               System.out.printf("\n");
               /* Input | Add to shoppingCart if selected */
               break;
 
           case 2: // Display: 2nd choice
-              System.out.printf("Option 3: Cheese ($.85)\n" + products.get(2).toString());
+              System.out.println("Option 2: Cheese ($.85)" + products.get(2).toString());
               System.out.printf("\n");
               /* Input | Add to shoppingCart if selected */
               break;
 
           case 3: // Display: 2nd choice
-              System.out.printf("Option 4: Eggs ($6.95)\n" + products.get(3).toString());
+              System.out.println("Option 3: Eggs ($6.95)" + products.get(3).toString());
               System.out.printf("\n");
               /* Input | Add to shoppingCart if selected */
               break;
 
           case 4: // Display: 2nd choice
-              System.out.printf("Option 5: Ice Cream ($4.95)\n" + products.get(4).toString());
+              System.out.println("Option 4: Ice Cream ($4.95)" + products.get(4).toString());
               System.out.printf("\n");
               /* Input | Add to shoppingCart if selected */
               break;
 
           case 5: // Display: 2nd choice
-              System.out.printf("Option 6: Poptarts ($3.49)\n" + products.get(5).toString());
+              System.out.println("Option 5: Poptarts ($3.49)" + products.get(5).toString());
               System.out.printf("\n");
               /* Input | Add to shoppingCart if selected */
               break;
 
           case 6: // Display: 2nd choice
-              System.out.printf("Option 7: Oreos ($5.99)\n" + products.get(6).toString());
+              System.out.println("Option 6: Oreos ($5.99)" + products.get(6).toString());
               System.out.printf("\n");
               /* Input | Add to shoppingCart if selected */
               break;

030d9e2cd7197dc719fc93579b3b67825ad9d45e
aks3632 <aks3632@mavs.uta.edu>
2023-02-14 18:59:24 -0600

    PO4 TrialRun12 2/14/23 6:59PM

--- P04/full_credit/Store.java
+++ P04/full_credit/Store.java
@@ -49,14 +49,14 @@ public class Store {
     System.out.println("Welcome to the Store");
     System.out.println("=====================\n");
     System.out.printf("Enter a choice: \n"); // Promt user
-    System.out.println("1. "  + products.get(0).toString());  // (M)ilk || tfree.price()
-    System.out.println("2. "  + products.get(1).toString());  // (B)read
-    System.out.println("3. "  + products.get(2).toString());  // (C)heese
-    System.out.println("4. "  + products.get(3).toString());  // (E)ggs
-    System.out.println("5. "  + products.get(4).toString());  // (I)ce Cream
-    System.out.println("6. "  + products.get(5).toString());  // (P)optarts
-    System.out.println("7. "  + products.get(6).toString());  // (O)reos
-    System.out.println("8. (q)uit?\n");
+    System.out.println("0. "  + products.get(0).toString());  // (M)ilk || tfree.price()
+    System.out.println("1. "  + products.get(1).toString());  // (B)read
+    System.out.println("2. "  + products.get(2).toString());  // (C)heese
+    System.out.println("3. "  + products.get(3).toString());  // (E)ggs
+    System.out.println("4. "  + products.get(4).toString());  // (I)ce Cream
+    System.out.println("5. "  + products.get(5).toString());  // (P)optarts
+    System.out.println("6. "  + products.get(6).toString());  // (O)reos
+    System.out.println("7. (q)uit?\n");
     System.out.printf("> Chose a number: ");
     Scanner number = new Scanner(System.in);
 
@@ -69,14 +69,14 @@ public class Store {
         System.err.println(e.getMessage());
         System.exit(-1);
       } // end try...catch
-      if (input <0 || input > 8)
+      if (input <0 || input > 7)
       {
         throw new IllegalArgumentException("\nInvalid menu choice. Good bye!");
         // System.out.println("\n Invalid menu choice please try again");
       } // end if
       return input;
 
-    } while (input < 0 || input > 8);
+    } while (input < 0 || input > 7);
   } // end menu
 
 
@@ -94,49 +94,49 @@ public class Store {
 
         System.out.printf("\n");
         switch (choice) {
-          case 1: // Dispaly: Dataset list
+          case 0: // Dispaly: Dataset list
               System.out.println("Option 1: Milk ($2.85)" + products.get(0).toString());
               System.out.printf("\n");
               /* Input | Add to shoppingCart if selected */
               break;
 
-          case 2: // Display: 2nd choice
+          case 1: // Display: 2nd choice
               System.out.printf("Option 2: Bread ($1.99)\n" + products.get(1).toString());
               System.out.printf("\n");
               /* Input | Add to shoppingCart if selected */
               break;
 
-          case 3: // Display: 2nd choice
+          case 2: // Display: 2nd choice
               System.out.printf("Option 3: Cheese ($.85)\n" + products.get(2).toString());
               System.out.printf("\n");
               /* Input | Add to shoppingCart if selected */
               break;
 
-          case 4: // Display: 2nd choice
+          case 3: // Display: 2nd choice
               System.out.printf("Option 4: Eggs ($6.95)\n" + products.get(3).toString());
               System.out.printf("\n");
               /* Input | Add to shoppingCart if selected */
               break;
 
-          case 5: // Display: 2nd choice
+          case 4: // Display: 2nd choice
               System.out.printf("Option 5: Ice Cream ($4.95)\n" + products.get(4).toString());
               System.out.printf("\n");
               /* Input | Add to shoppingCart if selected */
               break;
 
-          case 6: // Display: 2nd choice
+          case 5: // Display: 2nd choice
               System.out.printf("Option 6: Poptarts ($3.49)\n" + products.get(5).toString());
               System.out.printf("\n");
               /* Input | Add to shoppingCart if selected */
               break;
 
-          case 7: // Display: 2nd choice
+          case 6: // Display: 2nd choice
               System.out.printf("Option 7: Oreos ($5.99)\n" + products.get(6).toString());
               System.out.printf("\n");
               /* Input | Add to shoppingCart if selected */
               break;
 
-          case 8: // Gracefully terminate program
+          case 7: // Gracefully terminate program
               System.out.printf("Good Bye!\n");
               System.out.printf("\n");
               System.exit(-1);
@@ -144,11 +144,11 @@ public class Store {
 
           default:
               System.out.printf("%s\n%s\n", "Invalid input!",
-              "Please enter a number range (1 - 8).\n"); // Key selection is NOT an integer.
+              "Please enter a number range (0 - 7).\n"); // Key selection is NOT an integer.
               break;
         } // end switch
         /* code */
-      } while((choice != 8)); // end do..while |  || (choice != '4')
+      } while((choice != 7)); // end do..while |  || (choice != '4')
 
   } // end start()
 

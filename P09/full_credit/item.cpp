c144d7cff98b468fd2cf8368744bc27a43e0f709
aks3632 <aks3632@mavs.uta.edu>
2023-04-11 02:44:46 -0500

    P09 Trial Run2

--- P09/full_credit/item.cpp
+++ P09/full_credit/item.cpp
@@ -1,12 +1,12 @@
 #include "item.h"
+
 #include <iostream>
 #include <sstream>
 #include <string>
 
-class Item {
   // The constructor stores the name parameter into the _name private variable
   //                    and the price parameter into the _price private variable
-  public : Item(std::string name, int price) : _name{name}, _price{price} {
+  Item::Item(std::string name, int price) : _name{name}, _price{price} {
     if(_price < 0) { // Validate or check data NOT negative
       throw std::runtime_error("Invalid - price is negative!" + std::to_string(_price));
     }
@@ -14,16 +14,14 @@ class Item {
 
   // The default constructor delegates (or chains) to the non-default
   //    constructor, allowing "Item itcl;" declarations
-  Item() : Item{"",0} { }
+  Item::Item() : Item{"",0} { }
 
   // Java "" + name + " " + "($" + cost + ")";
-  std::string to_string() {
+  std::string Item::to_string() {
     // Item item{"Cookies", 195};
     std::string str = _name + " ($ " + std::to_string(_price) + ")";
     return str;
   }
 
-private:
-  std::string _name; // Field or variable - AKS
-  int _price;        // Field or variable - AKS
-};
+  // std::string Item::_name; // Field or variable - AKS
+  // int Item::_price;        // Field or variable - AKS
--- P09/full_credit/vend.cpp
+++ P09/full_credit/vend.cpp
@@ -4,6 +4,7 @@
 
 int main(int argc, char** argv) {
 
+  // Item itcl
   Item item{"Cookies", 195};
 
   std::cout << "This is main!" << "Item = " << item.to_string() << std::ends;

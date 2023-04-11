cc3a73ae32e34363902ef6f05f3e2ffed7c179bd
aks3632 <aks3632@mavs.uta.edu>
2023-04-11 07:20:50 -0500

    P09 Trial Run4 7:20AM 4/11/23

--- P09/full_credit/vend.cpp
+++ P09/full_credit/vend.cpp
@@ -5,8 +5,11 @@
 int main(int argc, char** argv) {
 
   // Item itcl
-  Item item{"Cookies", 195};
+  // Item item{"Cookies", 195};
+
+  std::cout << "======================" << std::endl;
+  std::cout << "Welcome to UTA Vending" << std::endl;
+  std::cout << "======================" << std::endl;
 
   std::cout << "This is main!" << "Item = " << item.to_string() << std::ends;
-
 }
